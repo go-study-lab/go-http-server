30dd7d99c0b9744d34616cb794942c6937d0c366
kevinYan <hengjia_yan@qingsongchou.com>
2020-02-04 18:10:47 +0800

    basic http server with graceful shutdown

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", &helloHandler{})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// 创建系统信号接收器
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-done
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server:", err)
+		}
+	}()
+
+	log.Println("Starting HTTP server...")
+	err := server.ListenAndServe()
+	if err != nil {
+		if err == http.ErrServerClosed {
+			log.Print("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
+	}
+}
+
+type helloHandler struct{}
+
+func (*helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
