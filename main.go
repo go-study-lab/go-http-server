ea4a0d4104d5786b4c340c035192d2d8b3a50b98
kevinYan <hengjia_yan@qingsongchou.com>
2020-02-05 16:24:12 +0800

    import gorilla/mux into project

--- main.go
+++ main.go
@@ -8,15 +8,33 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/gorilla/mux"
 )
 
+type helloHandler struct{}
+
+func (*helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", &helloHandler{})
+	//mux := http.NewServeMux()
+	//mux.Handle("/", &helloHandler{})
+	router := mux.NewRouter()
+
+	router.HandleFunc("/names/{name}/countries/{country}", func(writer http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		name := vars["name"]
+		country := vars["country"]
+		fmt.Fprintf(writer, "This guy named %s, was coming from %s .", name, country)
+	})
+
+	router.Handle("/", &helloHandler{})
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: mux,
+		Handler: router,
 	}
 
 	// 创建系统信号接收器
@@ -41,8 +59,3 @@ func main() {
 	}
 }
 
-type helloHandler struct{}
-
-func (*helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
